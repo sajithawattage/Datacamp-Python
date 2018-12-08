d09657468a792d6450be15ed904a2b5ad7327c37
Sajitha Wattage <sajitha.wattage@gmail.com>
2018-12-09 02:35:15 +0800

    inital python checking

---
+++ script.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+savings = 100
+
+savings = savings + 100
+
+print(savings)
+
+
+#list examples
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list ares
+#areas = [11.25, 18.0,20.0,10.75,9.50]
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
